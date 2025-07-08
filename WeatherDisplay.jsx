6aa3140c7c87e63a7f7957b955456b3fecee2f39
Iffat Naseem <ifatyasinengineer30@gmail.com>
2025-07-08 09:36:00 +0500

    Update WeatherDisplay.jsx

--- WeatherDisplay.jsx
+++ WeatherDisplay.jsx
@@ -4,13 +4,13 @@ function WeatherDisplay({ data }) {
   return (
     <div style={{ marginTop: "20px" }}>
       <h2>
-        📍 {data.name}, {data.sys.country}
+         {data.name}, {data.sys.country}
       </h2>
-      <p>🌡️ Temperature: {data.main.temp}°C</p>
-      <p>🤔 Feels like: {data.main.feels_like}°C</p>
-      <p>🌥️ Weather: {data.weather[0].description}</p>
-      <p>💧 Humidity: {data.main.humidity}%</p>
-      <p>🌬️ Wind Speed: {data.wind.speed} m/s</p>
+      <p> Temperature: {data.main.temp}°C</p>
+      <p> Feels like: {data.main.feels_like}°C</p>
+      <p> Weather: {data.weather[0].description}</p>
+      <p>Humidity: {data.main.humidity}%</p>
+      <p> Wind Speed: {data.wind.speed} m/s</p>
     </div>
   );
 }
